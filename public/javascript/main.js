0c144a8ecedd8f8a5279c4083d4930129666670d
Mankirat Singh <mankirat1998@gmail.com>
2017-10-14 21:44:40 +0530

    feat: Edit comment added

---
+++ public/javascript/main.js
@@ -0,0 +1,27 @@
+"use strict";
+
+
+// $('#edit-link').on('click', function(e){ //
+//     window.alert("This is alert");
+//     e.preventDefault();
+//     var $parent = $(this).parent(); // or var $parent = $(this).parent();
+//     $parent.find('p.message').hide(); // use slide methods fancy show/hide are required
+//     $parent.find('form.editForm').show();
+//     // Not sure but you'd want to hide <a href="#">Edit link </a> after clicking
+//     // in that case use the below
+//     // $(this).hide();
+// });
+
+$(document).ready(function() {
+
+    $(".edit-link").on("click", function (e) {
+        var $parent = $(this).parent(); // or var $parent = $(this).parent();
+        $parent.find('p.message').hide(); // use slide methods fancy show/hide are required
+        $parent.find('form.editForm').show();
+        $(this).hide();
+        // Not sure but you'd want to hide <a href="#">Edit link </a> after clicking
+        // in that case use the below
+        // $(this).hide();
+    });
+
+});
--- routes/comments.js
+++ routes/comments.js
@@ -48,6 +48,38 @@ router.post("/",isLoggedIn ,function (req, res) {
     });
 });
 
+router.put("/:comm_id", function (req, res) {
+    Comment.findByIdAndUpdate(req.params.comm_id, req.body.comment, function (err, updatedComment) {
+        if(err){
+            return res.redirect("back");
+        }
+        res.redirect("/campgrounds/"+req.params.id);
+    });
+});
+
+router.delete("/:comm_id", function (req, res) {
+
+})
+
+function checkCommentOwnership(req,res,next) {
+    if(req.isAuthenticated()){
+        Comment.findById(req.params.id, function (err, camp) {
+            if(err){
+                console.log(err);
+                return res.redirect("back");
+            }else{
+                if(camp.author.id.equals(req.user._id)){
+                    next();
+                }else{
+                    res.redirect("back");
+                }
+            }
+        });
+    }else{
+        res.redirect("back");
+    }
+}
+
 //middleware
 function isLoggedIn(req,res,next) {
     if(req.isAuthenticated()){
